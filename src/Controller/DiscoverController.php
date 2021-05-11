a9526075b025e075f56bbd3e69680f3920bdc054
Séverine <severine.coute@laposte.net>
2021-05-11 23:32:32 +0200

    discover_page_creation

---
+++ src/Controller/DiscoverController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\HttpClient\HttpClient;
+class DiscoverController extends AbstractController
+{
+
+public function index()
+{
+    $news = [];
+    $client = HttpClient::create();
+    $response = $client->request(
+        'GET',
+        'https://test.spaceflightnewsapi.net/api/v2/articles'
+    );
+    // TODO: change link to get more content
+
+    $statusCode = $response->getStatusCode(); // get Response status code 200
+
+    if ($statusCode === 200) {
+        $news = $response->getContent();
+        // get the response in JSON format
+        $news = $response->toArray();
+        // convert the response (here in JSON) to an PHP array
+    }
+    // var_dump($news);die;
+    return $this->twig->render('Discover/index.html.twig', ['news' => $news]);
+}
+}
+
