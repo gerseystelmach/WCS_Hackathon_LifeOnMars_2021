f7a0cdc59739a4d73ed6d35636aac37871df6f38
Séverine <severine.coute@laposte.net>
2021-05-12 10:45:25 +0200

    switch weather and news and change image planet

--- src/Controller/DiscoverController.php
+++ src/Controller/DiscoverController.php
@@ -25,22 +25,6 @@ class DiscoverController extends AbstractController
             $news = $response->toArray();
             // convert the response (here in JSON) to an PHP array
         }
-
-        $response = $client->request(
-            'GET',
-            'https://api.nasa.gov/insight_weather/?api_key=' . APP_KEY . '&feedtype=json&ver=1.0'
-        );
-        // TODO: change link to get more content
-
-        $statusCode = $response->getStatusCode(); // get Response status code 200
-
-        if ($statusCode === 200) {
-            $weather = $response->getContent();
-            // get the response in JSON format
-            $weather = $response->toArray();
-            // convert the response (here in JSON) to an PHP array
-        }
-
         //var_dump($map);die;
         return $this->twig->render('Discover/index.html.twig', ['news' => $news, 'weather' => $weather]);
     }
